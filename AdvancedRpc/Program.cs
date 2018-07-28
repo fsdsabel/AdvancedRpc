e10e161140050c0909ac38e4dc896843b9d1be6c
Daniel Sabel <webmaster@danielsabel.de>
2018-07-28 10:54:13 +0200

    Add project files.

---
+++ AdvancedRpc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedRpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
